a590a29443cb9d2450f813fed31fc9fa7dc911cf
YamanoiYasushi <wiz.yamanoi@wiz.ac.jp>
2020-06-17 01:56:53 +0900

    Html2XML修正

--- Html2Xml/Html2Xml/Html2Xml.cpp
+++ Html2Xml/Html2Xml/Html2Xml.cpp
@@ -8,15 +8,33 @@
 #include <vector>
 #include <map>
 #include <cctype>
+#include <regex>
+
+#include <SDKDDKVer.h>
+//#include <atlbase.h>
+//#include <atlstr.h>
+#include <afxinet.h>
+
 
 using namespace std;
+
+
+//tgtをchkを検索してchangeに置き換える関数
+void StrReplace(string& tgt, const string& chk, const string& change) {
+	auto index = tgt.find(chk);
+	while (index != string::npos) {
+		tgt.replace(index, chk.size(), change);
+		index = tgt.find(chk, index + change.size());
+	}
+}
+
 
 
 //開始タグ名を取り出す
 string getStartTagName(const string& tag) {
 	bool in = false;
 	string ret = "";
-	for (int i = 1; i < tag.size(); i++) {
+	for (size_t i = 1; i < tag.size(); i++) {
 		if (in) {
 			if (!isalpha(tag[i])) {
 				return ret;
@@ -43,7 +61,7 @@ string getStartTagName(const string& tag) {
 string getEndTagName(const string& tag) {
 	bool in = false;
 	string ret = "";
-	for (int i = 2; i < tag.size(); i++) {
+	for (size_t i = 2; i < tag.size(); i++) {
 		if (in) {
 			if (!isalpha(tag[i])) {
 				return ret;
@@ -127,7 +145,7 @@ struct KeyValue {
 	}
 };
 
-void CreateAttlibutes(Node& newNode,const vector<string>& tagStrings) {
+void CreateAttlibutes(Node& newNode, const vector<string>& tagStrings) {
 	for (size_t i = 1; i < tagStrings.size(); i++) {
 		KeyValue keyval;
 		keyval.Create(tagStrings[i]);
@@ -149,7 +167,7 @@ Node CreateSingleTagNode(const string& tagStr) {
 	vector<string> tagStrings;
 	size_t index = 0;
 	string ret = "";
-	for (int i = 1; i < tagStr.size(); i++) {
+	for (size_t i = 1; i < tagStr.size(); i++) {
 		if (in) {
 			if (inin) {
 				if (tagStr[i] == '"') {
@@ -179,7 +197,7 @@ Node CreateSingleTagNode(const string& tagStr) {
 			}
 		}
 		else {
-			if (tagStr[i] != ' ' &&  tagStr[i] != '\t' && tagStr[i] != '\n') {
+			if (tagStr[i] != ' ' && tagStr[i] != '\t' && tagStr[i] != '\n') {
 				auto count = tagStrings.size();
 				tagStrings.push_back(string());
 				index = count;
@@ -226,35 +244,83 @@ bool ChkSimpleTag(const string& tagName) {
 }
 
 
-bool ChkOmissionTag(const string& tagName) {
-	string chk = tagName;
-	transform(chk.begin(), chk.end(), chk.begin(), ::tolower);
-	vector<string> vec = {
-		"li",
-		"dt",
-		"dd",
-		"p",
-		"rt",
-		"rp",
-		"optgroup",
-		"option",
-		"colgroup",
-		"thead",
-		"tbody",
-		"tfoot"
-		"tr",
-		"td",
-		"th"
-	};
-	for (auto& v : vec) {
-		if (chk == v) {
+bool ChkOmissionTag(const string& tagName, const string& nextTagName) {
+	string chk1 = tagName;
+	transform(chk1.begin(), chk1.end(), chk1.begin(), ::tolower);
+	string chk2 = nextTagName;
+	transform(chk2.begin(), chk2.end(), chk2.begin(), ::tolower);
+	if (chk1 == "li" && chk2 == "li") {
+		return true;
+	}
+	if (chk1 == "dt") {
+		if (chk2 == "dt" || chk2 == "dd") {
 			return true;
+		}
+	}
+	if (chk1 == "dd") {
+		if (chk2 == "dt" || chk2 == "dd") {
+			return true;
+		}
+	}
+	if (chk1 == "rt") {
+		if (chk2 == "rt" || chk2 == "rp") {
+			return true;
+		}
+	}
+	if (chk1 == "rp") {
+		if (chk2 == "rt" || chk2 == "rp") {
+			return true;
+		}
+	}
+	if (chk1 == "optgroup" && chk2 == "optgroup") {
+		return true;
+	}
+	if (chk1 == "option") {
+		if (chk2 == "option" || chk2 == "optgroup") {
+			return true;
+		}
+	}
+	if (chk1 == "thead") {
+		if (chk2 == "tbody" || chk2 == "tfoot") {
+			return true;
+		}
+	}
+	if (chk1 == "tbody") {
+		if (chk2 == "tbody" || chk2 == "tfoot") {
+			return true;
+		}
+	}
+	if (chk1 == "td") {
+		if (chk2 == "td" || chk2 == "th") {
+			return true;
+		}
+	}
+	if (chk1 == "th") {
+		if (chk2 == "td" || chk2 == "th") {
+			return true;
+		}
+	}
+	//pタグ
+	vector<string> vecChkP = {
+		"address","article","aside",
+		"blockquote","details","div","dl",
+		"fieldset","figcaption","figure",
+		"footer","form",
+		"h1","h2","h3","h4","h5","h6",
+		"header","hgroup","hr",
+		"main","menu","nav","ol","p","pre","section","table","ul"
+	};
+	if (chk1 == "p") {
+		for (auto& v : vecChkP) {
+			if (chk2 == v) {
+				return true;
+			}
 		}
 	}
 	return false;
 }
 
-size_t CreateNodes(Node& node,size_t tokenIndex) {
+size_t CreateNodes(Node& node, size_t tokenIndex) {
 	while (tokenVec.size() > tokenIndex) {
 		if (isStartTag(tokenVec[tokenIndex])) {
 			string startTagName = getStartTagName(tokenVec[tokenIndex]);
@@ -262,6 +328,11 @@ size_t CreateNodes(Node& node,size_t tokenIndex) {
 				//コメントやDocType宣言
 				tokenIndex++;
 				continue;
+			}
+			if (ChkOmissionTag(node.m_nodeName, startTagName)) {
+				//省略可能なタグだった
+				//tokenIndexは進めずにリターン
+				return tokenIndex;
 			}
 			Node newNode = CreateSingleTagNode(tokenVec[tokenIndex]);
 			if (ChkSimpleTag(newNode.m_nodeName)) {
@@ -300,7 +371,7 @@ size_t CreateNodes(Node& node,size_t tokenIndex) {
 void CreateTokens(const string& html) {
 	bool textFlg = true;
 	string temp;
-	for (int i = 0; i < html.size(); i++) {
+	for (size_t i = 0; i < html.size(); i++) {
 		if (textFlg) {
 			if (html[i] == '<') {
 				if (temp != "") {
@@ -330,70 +401,150 @@ void CreateTokens(const string& html) {
 
 }
 
-void OutNodes(const Node& node) {
-	if (node.m_type == NodeType::typeRoot|| node.m_type == NodeType::typeTag) {
+void OutNodes(const Node& node,ofstream& ofs) {
+	if (node.m_type == NodeType::typeRoot || node.m_type == NodeType::typeTag) {
 		if (node.m_type == NodeType::typeRoot) {
-			cout << "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+			ofs << "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
 		}
 		if (node.m_nodeName != "") {
-			cout << '<' << node.m_nodeName;
+			ofs << '<' << node.m_nodeName;
 			if (node.m_attlibutes.size() > 0) {
 				auto it = node.m_attlibutes.begin();
 				while (it != node.m_attlibutes.end()) {
-					cout << ' ' << (*it).first << '=' << '"' << (*it).second << '"';
+					ofs << ' ' << (*it).first << '=' << '"' << (*it).second << '"';
 					it++;
 				}
 			}
-			cout << '>';
+			ofs << '>';
 		}
 		size_t i = 0;
 		while (i < node.m_children.size()) {
 			if (node.m_children[i].m_type == NodeType::typeTag) {
-				OutNodes(node.m_children[i]);
+				OutNodes(node.m_children[i], ofs);
 			}
 			else if (node.m_children[i].m_type == NodeType::typeSimpleTag) {
-				cout << '<' << node.m_children[i].m_nodeName;
+				ofs << '<' << node.m_children[i].m_nodeName;
 				if (node.m_children[i].m_attlibutes.size() > 0) {
 					auto it = node.m_children[i].m_attlibutes.begin();
 					while (it != node.m_children[i].m_attlibutes.end()) {
-						cout << ' ' << (*it).first << '=' << '"' << (*it).second << '"';
+						ofs << ' ' << (*it).first << '=' << '"' << (*it).second << '"';
 						it++;
 					}
 				}
-				cout << " />";
+				ofs << " />";
 			}
 			else if (node.m_children[i].m_type == NodeType::typeText) {
-				cout << node.m_children[i].m_text;
+				ofs << node.m_children[i].m_text;
 			}
 			i++;
 		}
 		if (node.m_nodeName != "") {
-			cout << "</" << node.m_nodeName << '>';
+			ofs << "</" << node.m_nodeName << '>';
 		}
 	}
 }
-/*
-閉じタグ無し
-area、base、br、col、command、embed、hr、img、input、keygen、link、meta、param、source、track、wbr
-*/
-
-/*
-終了タグを省略できる要素
-li、dt、dd、p、rt、rp、optgroup、option、colgroup、thead、tbody、tfoot、tr、td、th
-*/
 
 
-int main()
+void GetHttpPage(LPCTSTR pszServerName, LPCTSTR pszFileName,int port,string& ret)
 {
-	ifstream ifs("test.html");
-	string line;
-	string html;
-	while (getline(ifs, line)) {
-		html += line;
-		html += "\n";
+	CInternetSession session(_T("My Session"));
+	CHttpConnection* pServer = NULL;
+	CHttpFile* pFile = NULL;
+	try
+	{
+		CString strServerName;
+		INTERNET_PORT nPort = port;
+		DWORD dwRet = 0;
+
+		pServer = session.GetHttpConnection(pszServerName, nPort);
+		pFile = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, pszFileName);
+		pFile->SendRequest();
+		pFile->QueryInfoStatusCode(dwRet);
+
+
+
+		if (dwRet == HTTP_STATUS_OK)
+		{
+			CHAR szBuff[1024];
+			while (pFile->Read(szBuff, 1024) > 0)
+			{
+				ret += szBuff;
+			}
+		}
+		delete pFile;
+		delete pServer;
 	}
-	CreateTokens(html);
-	CreateNodes(rootNode, 0);
-	OutNodes(rootNode);
+	catch (CInternetException* pEx)
+	{
+		//catch errors from WinInet
+		TCHAR pszError[64];
+		pEx->GetErrorMessage(pszError, 64);
+		_tprintf_s(_T("%63s"), pszError);
+	}
+	session.Close();
+}
+
+void SplitURL2ServerFile(const string& base,string& server,string& file) {
+	server.clear();
+	file.clear();
+	auto sz = base.find(string("://"));
+	sz += 3;
+	bool flg = false;
+	while (sz < base.size()) {
+		if (base[sz] == '/') {
+			flg = true;
+		}
+		if (flg) {
+			file += base[sz];
+		}
+		else {
+			server += base[sz];
+		}
+		sz++;
+	}
+}
+
+int main(int argc,char* argv[])
+{
+	if (argc == 3 || argc == 4) {
+		string inHtml = argv[1];
+		cout << inHtml << endl;
+		string html;
+		regex pat("^http.*");
+		if (regex_match(inHtml, pat)) {
+			//WEBから取り込む
+			string server, file;
+			SplitURL2ServerFile(inHtml, server, file);
+			if (file == "") {
+				file = "/";
+			}
+			CStringW serverW(server.c_str());
+			CStringW fileW(file.c_str());
+			int port = 80;
+			GetHttpPage(serverW, fileW, port, html);
+		}
+		else {
+			//ファイルから取り込む
+			ifstream ifs(inHtml);
+			string line;
+			while (getline(ifs, line)) {
+				html += line;
+				html += "\n";
+			}
+		}
+
+		CreateTokens(html);
+		CreateNodes(rootNode, 0);
+		ofstream outputfile(argv[2]);
+		OutNodes(rootNode, outputfile);
+		outputfile.close();
+		cout << argv[2] << "に保存されました" << endl;
+	}
+	else {
+		cout << "パラメータが足りません\n";
+		cout << "Html2Xml 読み込むHTML 書きだすXMLファイル [エンコード]" << endl;
+		return 0;
+	}
+	return 0;
 }
 
